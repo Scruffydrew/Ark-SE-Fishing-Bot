32f2933e04324521428ff4c06a3e93601aca01b8
Scruffydrew <ldhe1@ymail.com>
2021-11-25 14:44:28 +1100

    Changes pixel location for Z

--- Ark Fishing Bot/Ark_Fishing_Bot.py
+++ Ark Fishing Bot/Ark_Fishing_Bot.py
@@ -45,7 +45,7 @@ while loop == 1:
     PressW = PIL.ImageGrab.grab().load()[1113, 868]  # Get the RGB value for the pixel at x=1113, y=868 on your screen
     PressA = PIL.ImageGrab.grab().load()[1162, 970]  # Get the RGB value for the pixel at x=1162, y=970 on your screen
     PressD = PIL.ImageGrab.grab().load()[1187, 907]  # Get the RGB value for the pixel at x=1192, y=906 on your screen
-    PressZ = PIL.ImageGrab.grab().load()[1158, 973]  # Get the RGB value for the pixel at x=1158, y=973 on your screen
+    PressZ = PIL.ImageGrab.grab().load()[1151, 973]  # Get the RGB value for the pixel at x=1158, y=973 on your screen
     PressX = PIL.ImageGrab.grab().load()[1167, 972]  # Get the RGB value for the pixel at x=1167, y=972 on your screen
 
     PressE = PIL.ImageGrab.grab().load()[1186, 998]  # Get the RGB value for the pixel at x=1186, y=998 on your screen
